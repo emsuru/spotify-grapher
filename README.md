155167d6cb0d2742da7c24548a7f380b7c13b54f
emsuru <miruna.suru@gmail.com>
2024-02-18 17:35:02 +0100

    initialized repo

---
+++ README.md
@@ -0,0 +1,38 @@
+# Spotify Streaming Insights
+
+## 📖 Description
+
+Spotify Insight is a simple Python project that generates visualizations of your Spotify streaming history.
+
+## Features
+- Generate a bar chart of the top 10 songs played.
+- Generate a line chart of the listening history for the top 5 artists.
+
+## 🛠️ Setup & Installation
+To set up the project, clone the repository and install the required dependencies:
+
+```
+git clone https://github.com/emsuru/spotify-profiler.git
+```
+
+## 👩‍💻 Usage
+
+To run the script and generate your own graphs, copy the zip you get from Spotify into this project's directory (it will be called 'my_spotify_data.zip') then execute:
+
+```
+
+python main.py
+
+```
+
+The graphs will be saved as `top_10_songs.png` and `top_5_artists.png`.
+
+## 📂 Project background
+This is my first personal project after starting to learn Python and data analysis. My main goal with this is to practice working with Pandas data frames.
+
+The project is inspired by Eric Chiang's [original inquiry into his own streaming history](https://ericchiang.github.io/post/spotify/)
+
+Eric had access to his entire Spotify streaming history. I'm still waiting for mine (it takes up to 30 days) but meanwhile I got a small data set from Spotify, covering only the past year and a subset of info. 16,490 events iso the 120,000 Eric had.
+
+## License
+This project is open source and available under the [MIT License](LICENSE).
---
+++ main.py
@@ -0,0 +1,58 @@
+import datetime
+import zipfile
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+from matplotlib.ticker import FuncFormatter
+
+dfs = []
+zf = zipfile.ZipFile('my_spotify_data.zip', 'r')
+for name in zf.namelist():
+    if 'StreamingHistory' not in name:
+        continue
+    dfs.append(pd.read_json(zf.open(name)))
+
+df = pd.concat(dfs)
+
+print("Columns:", df.columns.values)
+print("Number of streams:", str(df.shape[0]))
+#print(datetime.timedelta(milliseconds=int(df['ms_played'].sum())))
+def format_ms(x, pos):
+    return str(round(x/(3.6e+6), 2)) + 'h'
+
+# Filter by year, group by artist, sum time played and take top ten.
+year_df = df.loc[pd.to_datetime(df['endTime']).apply(lambda t: t.year == 2023)]
+year_df = year_df.groupby(['trackName'])[['msPlayed']].sum()
+year_df = year_df.sort_values('msPlayed', ascending=False)
+year_df = year_df[:10]
+
+fig, ax = plt.subplots()
+
+plt.gca().xaxis.set_major_formatter(format_ms)
+ax.barh(year_df.index.values, year_df['msPlayed'])
+ax.set_title('Top 10 songs (Jan 2023 - Feb 2024)')
+ax.invert_yaxis()
+plt.savefig('top_10_songs.png', dpi=300, bbox_inches='tight')
+plt.show()
+
+
+# Determine top artists listened to.
+top_artists_df = df.groupby(['artistName'])[['msPlayed']]
+top_artists_df = top_artists_df.sum().sort_values('msPlayed', ascending=False)[:5]
+top_artists = [artist for (artist) in top_artists_df.index.values]
+
+fig, ax = plt.subplots(figsize=(10, 5))
+for artist in top_artists:
+  # Generate rolling sums of total amount of time listening to the artist.
+  df2 = df.loc[df['artistName'] == artist][['endTime', 'msPlayed']]
+  df2['endTime'] = df2['endTime'].apply(pd.to_datetime)
+  df2 = df2.sort_values('endTime')
+  df2 = df2.rolling('365D', on='endTime').sum()
+  ax.plot(df2['endTime'], df2['msPlayed'], label=artist)
+
+ax.set_title('Top 5 artists (Jan 2023 - Feb 2024)')
+plt.gca().yaxis.set_major_formatter(format_ms)
+plt.legend()
+plt.savefig('top_5_artists.png', dpi=300, bbox_inches='tight')
+plt.show()
